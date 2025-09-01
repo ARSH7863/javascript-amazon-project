eadc191fa4b597a7c7bc016a8ce5573fbe7f2968
ARSH <arshs287@gmail.com>
2025-09-01 13:16:50 +0530

    Delete button working

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -14,10 +14,12 @@ cart.forEach((cartItem) => {
       matchingProduct = product;
     }
   });
-  console.log(matchingProduct);
+  //   console.log(matchingProduct);
 
   cartSummaryHTML += `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${
+      matchingProduct.id
+    }">
     <div class="delivery-date">
         Delivery date: Tuesday, June 21
     </div>
@@ -104,6 +106,11 @@ document.querySelectorAll(".js-delete-link").forEach((link) => {
   link.addEventListener("click", () => {
     const productId = link.dataset.productId;
     removeCart(productId);
-    console.log(cart);
+
+    const container = document.querySelector(
+      `.js-cart-item-container-${productId}`
+    );
+    console.log(container);
+    container.remove();
   });
 });
