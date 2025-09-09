e79bc2d0ee486b05046ea4a6cae23749ed082b8a
ARSH <arshs287@gmail.com>
2025-09-09 16:38:17 +0530

    Did automated testings for currency

---
+++ scripts/tests/moneyTest.js
@@ -0,0 +1,25 @@
+import { formatCurrency } from "../utils/money.js";
+
+console.log(`Converts currency into dollars`);
+
+if (formatCurrency(2095) === "20.95") {
+  console.log(`passed`);
+} else {
+  console.log(`failed`);
+}
+
+console.log(`Works with 0`);
+
+if (formatCurrency(0) === "0.00") {
+  console.log(`passed`);
+} else {
+  console.log(`failed`);
+}
+
+console.log(`Rounds upto nearest cent`);
+
+if (formatCurrency(2000.5) === "20.01") {
+  console.log(`passed`);
+} else {
+  console.log(`failed`);
+}
