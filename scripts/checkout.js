d5e9d3a702f957cd08b85693eea6747a2ab9b8d8
ARSH <arshs287@gmail.com>
2025-09-25 05:22:18 +0530

    Promises applied for loading products and cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,24 +2,27 @@ import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts, loadProductsFetch } from "../data/products.js";
-import { loadCart } from "../data/cart.js";
+import { loadCart, loadCartFetch } from "../data/cart.js";
 
 // import "../data/cart-class.js";
 // import "../data/car.js";
 // import "../data/backend-practice.js";
 
 async function loadPage() {
-  await loadProductsFetch();
+  try {
+    // await new Promise((resolve) => {
+    //   loadCart(() => {
+    //     resolve();
+    //   });
+    // });
+    await Promise.all([loadProductsFetch(), loadCartFetch()]);
 
-  await new Promise((resolve) => {
-    loadCart(() => {
-      resolve();
-    });
-  });
-
-  renderCheckoutHeader();
-  renderOrderSummary();
-  renderPaymentSummary();
+    renderCheckoutHeader();
+    renderOrderSummary();
+    renderPaymentSummary();
+  } catch (error) {
+    console.log(`Unexpected Error. Please try again later`, error);
+  }
 }
 loadPage();
 
